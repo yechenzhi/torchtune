6a1bf44f70b72c65f3a268cced98b49657fbcc89
yechenzhi <136920488@qq.com>
2024-04-24 10:41:09 +0800

    add ufc dataset

--- torchtune/datasets/__init__.py
+++ torchtune/datasets/__init__.py
@@ -11,6 +11,7 @@ from torchtune.datasets._instruct import instruct_dataset, InstructDataset
 from torchtune.datasets._samsum import samsum_dataset
 from torchtune.datasets._slimorca import slimorca_dataset
 from torchtune.datasets._stack_exchanged_paired import stack_exchanged_paired_dataset
+from torchtune.datasets._ultrafeedback_cleaned import ultrafeedback_cleaned_dataset
 
 __all__ = [
     "alpaca_dataset",
@@ -23,4 +24,5 @@ __all__ = [
     "ChatDataset",
     "instruct_dataset",
     "chat_dataset",
+    "ultrafeedback_cleaned_dataset",
 ]
